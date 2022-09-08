130bc53438164a4daf355a8defba313ee4553fee
Manuel Reichel <44375107+manureichel@users.noreply.github.com>
2022-09-08 19:49:31 -0300

    agrego Loader

--- src/containers/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer.jsx
@@ -3,22 +3,26 @@ import { useEffect } from "react";
 import { useState } from "react";
 import ItemDetail from "../components/items/ItemDetail";
 import { useParams } from "react-router-dom";
+import Loader from "../components/info/Loader";
 
 export default function ItemDetailContainer() {
   const [product, setProduct] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   const params = useParams();
 
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${params.productId}`)
       .then((response) => response.json())
-      .then((response) => setProduct(response))
+      .then((response) => {
+        setProduct(response);
+        setLoading(false);
+      })
       .catch((err) => console.error(err));
   }, [params]);
 
   return (
     <div className="hero mt-5">
-      <ItemDetail product={product} />
+      {!loading ? <ItemDetail product={product} /> : <Loader />}
     </div>
   );
 }
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -3,10 +3,12 @@ import { useEffect } from "react";
 // import ItemCount from "./ItemCount";
 import { useParams } from "react-router-dom";
 import ItemList from "../components/items/ItemList";
+import Loader from "../components/info/Loader";
 
 export default function ItemListContainer({ greetings }) {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const params = useParams();
   useEffect(() => {
@@ -15,6 +17,7 @@ export default function ItemListContainer({ greetings }) {
       .then((response) => {
         setProducts(response);
         setFilteredProducts(response);
+        setLoading(false);
       })
       .catch((err) => console.error(err));
   }, []);
@@ -32,7 +35,7 @@ export default function ItemListContainer({ greetings }) {
 
   return (
     <div className="flex flex-wrap justify-center bg-base-200 p-10 my-5 rounded-md">
-      <ItemList products={filteredProducts} />
+      {!loading ? <ItemList products={filteredProducts} /> : <Loader />}
     </div>
   );
 }
