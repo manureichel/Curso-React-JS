abc7ea28f7fcf50704667b477d55ea0fa5a2b677
Manuel Reichel <44375107+manureichel@users.noreply.github.com>
2022-09-13 00:30:36 -0300

    Sincronizar counter

--- src/components/items/ItemDetail.jsx
+++ src/components/items/ItemDetail.jsx
@@ -1,20 +1,54 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import ItemCount from "./ItemCount";
 
 export default function ItemDetail({ product }) {
+  const stock = 53;
+
+  const [quantity, setQuantity] = React.useState(0);
+
+  const navigate = useNavigate();
+
+  const handleAddToCart = (quantity) => {
+    setQuantity(quantity);
+  };
+
+  const handleFinish = () => {
+    navigate("/cart");
+  };
+
   return (
-    <div>
-      <div className="card w-96 bg-base-200 shadow-xl">
+    <div className="flex items-center card w-96 bg-base-200 shadow-xl">
+      <div className="">
         <figure className="px-10 pt-10">
-          <img src={product.image} alt={product.title} className="rounded-xl" />
+          <img
+            src={product.image}
+            alt={product.title}
+            className="rounded-xl max-h-72"
+          />
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">{product.title}</h2>
-          <p>{product.description}</p>
+          <p className="text-justify">{product.description}</p>
           <p>${product.price}</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Comprar</button>
-          </div>
         </div>
+      </div>
+
+      <div className="">
+        {!quantity ? (
+          <ItemCount
+            stock={stock}
+            initial={quantity}
+            onAddToCart={handleAddToCart}
+          />
+        ) : (
+          <button
+            className="btn btn-primary mb-3"
+            onClick={() => handleFinish()}
+          >
+            Terminar Compra
+          </button>
+        )}
       </div>
     </div>
   );
---
+++ src/containers/CartContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CartContainer() {
+  return (
+    <div className="hero min-h-screen bg-base-100">
+      <div className="text-center hero-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Cart Container</h1>
+          <p className="mb-5">Componente de prueba</p>
+        </div>
+      </div>
+    </div>
+  );
+}
