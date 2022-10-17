abd121d29972f99a40f7bd9f9b3366cfda514993
Manuel Reichel <manuelreichel@gmail.com>
2022-10-17 02:04:02 -0300

    Agrego validación con Formik

--- src/components/CheckoutModal/CheckoutModal.jsx
+++ src/components/CheckoutModal/CheckoutModal.jsx
@@ -7,6 +7,9 @@ import { CartContext } from "../../context/CartContext";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
+import * as yup from "yup";
+import { Formik } from "formik";
+
 const MySwal = withReactContent(Swal);
 
 export const CheckoutModal = () => {
@@ -40,10 +43,9 @@ export const CheckoutModal = () => {
     MySwal.fire("Orden generada", `Código de orden: ${id}`, "success");
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("enviando");
+  const sendToFirebase = async () => {
     setLoading(true);
+
     const docRef = await addDoc(collection(db, "orders"), checkoutData);
     if (docRef.id) {
       console.log("Orden creada con ID: ", docRef.id);
@@ -55,105 +57,154 @@ export const CheckoutModal = () => {
     }
   };
 
-  const handleInputChange = (e) => {
-    //const name = e.target.name
-    //const value = e.target.value
-    const { name, value } = e.target;
-
-    setCheckoutData({
-      ...checkoutData,
-      buyer: {
-        ...checkoutData.buyer,
-        [name]: value,
-      },
-    });
-  };
+  const yupSchema = yup
+    .object()
+    .shape({
+      name: yup.string().required("El nombre es obligatorio"),
+      lastname: yup.string().required("El apellido es obligatorio"),
+      phone: yup.number().required("El teléfono es obligatorio"),
+      mail: yup
+        .string()
+        .email("El mail debe ser válido")
+        .required("El mail es obligatorio"),
+      address: yup.string().required("La dirección es obligatoria"),
+    })
+    .required();
 
   return (
     <div>
-      {!loading ? (
-        <div>
-          <label htmlFor="my-modal-3" className="btn modal-button">
-            Finalizar
-          </label>
-
-          <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-          <div className="modal">
-            <div className="modal-box relative flex flex-col items-center">
-              <label
-                htmlFor="my-modal-3"
-                className="btn btn-sm btn-circle absolute right-2 top-2"
-              >
-                ✕
+      <Formik
+        initialValues={{
+          name: "",
+          lastname: "",
+          phone: "",
+          mail: "",
+          address: "",
+        }}
+        onSubmit={(values) => {
+          setCheckoutData({
+            ...checkoutData,
+            buyer: {
+              ...values,
+            },
+          });
+          sendToFirebase();
+        }}
+        validationSchema={yupSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isValid,
+          dirty,
+        }) =>
+          !loading ? (
+            <div>
+              <label htmlFor="my-modal-3" className="btn modal-button">
+                Finalizar
               </label>
-              <h3 className="text-lg font-bold">Checkout</h3>
-              <p className="py-4">Ingresá tus datos para el envio.</p>
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="label-text">Nombre</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  onChange={handleInputChange}
-                  placeholder="Ingresá tu nombre completo"
-                  className="input input-bordered w-full max-w-xs"
-                />
-                <label className="label">
-                  <span className="label-text">Apellido</span>
-                </label>
-                <input
-                  type="text"
-                  name="lastname"
-                  onChange={handleInputChange}
-                  placeholder="Ingresá tu apellido"
-                  className="input input-bordered w-full max-w-xs"
-                />
-                <label className="label">
-                  <span className="label-text">Dirección</span>
-                </label>
-                <input
-                  type="text"
-                  name="address"
-                  onChange={handleInputChange}
-                  placeholder="Ingresá tu direcicón"
-                  className="input input-bordered w-full max-w-xs"
-                />
-                <label className="label">
-                  <span className="label-text">Teléfono</span>
-                </label>
-                <input
-                  type="number"
-                  name="phone"
-                  onChange={handleInputChange}
-                  placeholder="Ingresá tu número de teléfono"
-                  className="input input-bordered w-full max-w-xs"
-                />
-                <label className="label">
-                  <span className="label-text">E-mail</span>
-                </label>
-                <input
-                  type="mail"
-                  name="mail"
-                  onChange={handleInputChange}
-                  placeholder="Ingresá tu correo electrónico"
-                  className="input input-bordered w-full max-w-xs"
-                />
-                <button
-                  className="btn btn-primary mt-5 "
-                  onClick={handleSubmit}
-                >
-                  Enviar
-                </button>
+
+              <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+              <div className="modal">
+                <div className="modal-box relative flex flex-col items-center">
+                  <label
+                    htmlFor="my-modal-3"
+                    className="btn btn-sm btn-circle absolute right-2 top-2"
+                  >
+                    ✕
+                  </label>
+                  <h3 className="text-lg font-bold">Checkout</h3>
+                  <p className="py-4">Ingresá tus datos para el envio.</p>
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                      <span className="label-text">Nombre</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      onChange={handleChange}
+                      value={values.name}
+                      onBlur={handleBlur}
+                      placeholder="Ingresá tu nombre completo"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                    {errors.name && touched.name && errors.name}
+                    <label className="label">
+                      <span className="label-text">Apellido</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="lastname"
+                      onChange={handleChange}
+                      value={values.lastname}
+                      onBlur={handleBlur}
+                      placeholder="Ingresá tu apellido"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                    {errors.lastname && touched.lastname && errors.lastname}
+                    <label className="label">
+                      <span className="label-text">Dirección</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="address"
+                      onChange={handleChange}
+                      value={values.address}
+                      onBlur={handleBlur}
+                      placeholder="Ingresá tu direcicón"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                    {errors.address && touched.address && errors.address}
+                    <label className="label">
+                      <span className="label-text">Teléfono</span>
+                    </label>
+                    <input
+                      type="number"
+                      name="phone"
+                      onChange={handleChange}
+                      value={values.phone}
+                      onBlur={handleBlur}
+                      placeholder="Ingresá tu número de teléfono"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                    {errors.phone && touched.phone && errors.phone}
+                    <label className="label">
+                      <span className="label-text">E-mail</span>
+                    </label>
+                    <input
+                      type="mail"
+                      name="mail"
+                      onChange={handleChange}
+                      values={values.mail}
+                      onBlur={handleBlur}
+                      placeholder="Ingresá tu correo electrónico"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                    {errors.mail && touched.mail && errors.mail}
+                    <button
+                      disabled={!isValid || !dirty}
+                      className="btn btn-primary mt-5 "
+                      type="submit"
+                      onClick={handleSubmit}
+                    >
+                      Enviar
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <Loader />
-        </div>
-      )}
+          ) : (
+            <div>
+              <Loader />
+            </div>
+          )
+        }
+      </Formik>
     </div>
   );
 };
